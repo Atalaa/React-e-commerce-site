2af1b69e84f945ccafd49dee76c472a96756f024
Atala <atala.rchidi@gmail.com>
2022-12-11 13:31:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import FloatingCart from "./Components/FloatingCart/FloatingCart";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import ProductShowcase from "./Pages/ProductShowcase/ProductShowcase";
+import Contact from "./Pages/Contact/Contact";
+import ShoppingCart from "./Pages/ShopppingCart/ShoppingCart";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <FloatingCart />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductShowcase />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shoppingCart" element={<ShoppingCart />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
